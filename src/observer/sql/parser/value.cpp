0bf594ef333d14ded8e751dcb88cf14c358e24c4
Varyhoo <837880374@qq.com>
2024-03-25 20:54:06 +0800

    123456

--- src/observer/sql/parser/value.cpp
+++ src/observer/sql/parser/value.cpp
@@ -17,7 +17,7 @@ See the Mulan PSL v2 for more details. */
 #include "common/lang/string.h"
 #include "common/log/log.h"
 #include <sstream>
-
+// 1145141919810
 const char *ATTR_TYPE_NAME[] = {"undefined", "chars", "ints", "floats", "booleans"};
 
 const char *attr_type_to_string(AttrType type)
